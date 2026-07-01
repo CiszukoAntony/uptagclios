91a7c327a43a2c7cf636b48387000d767f14b46b
Luisferrrr67 <luisfer223008@gmail.com>
2026-07-01 01:02:15 -0400

    fix(func_juego_adivina.py):Adivina numero - Luis Sanchez

--- src/main/python/func_juego_adivina.py
+++ src/main/python/func_juego_adivina.py
@@ -1,4 +1,5 @@
 import random
+import utils
 
 def juego_adivina_el_numero():
     """
@@ -19,7 +20,7 @@ def juego_adivina_el_numero():
         
         # Bienvenida
         print("=" * 45)
-        print(" ¡BIENVENIDO AL JUEGO DE ADIVINA EL NÚMERO! ")
+        print(f"{utils.ansi.GREEN} ¡BIENVENIDO AL JUEGO DE ADIVINA EL NÚMERO! ")
         print("=" * 45)
         print("He pensado un número entre 1 y 100.")
         print("Intenta adivinarlo en el menor número de intentos posible.\n")
